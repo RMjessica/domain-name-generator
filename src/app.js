e85bc24b7a7538a12d612a59e273bfcae4f05519
RMjessica <jeess.rm1@gmail.com>
2022-06-12 11:19:16 +0000

    initial commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //Set up arrays
+  let pro = ["the", "our", "my", ""];
+  let adj = ["great", "big", "cool", "cheesy", "funny", "spectacular"];
+  let noun = ["jogger", "racoon", "giraffe", "witch", "tree", "cheese"];
+
+  //create function to map and return a domain name
+  let getDomName = arr => {
+    let a = Math.floor(Math.random() * arr.length);
+    return arr[a];
+  };
+
+  document.getElementById("theId").innerHTML =
+    getDomName(pro) + getDomName(adj) + getDomName(noun) + ".com";
+
+  //console.log(domGenFunction(pro, adj, noun).join("\n"));
+  //domGen.innerHTML = domGenFunction(pro, adj, noun).join("<br/>");
+
+  //document.getElementById("theId").innerHTML = dom;
 };
