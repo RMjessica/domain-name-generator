25dc12a16236cb62ae07dd28681df743b711f5cf
RMjessica <jeess.rm1@gmail.com>
2022-06-12 17:12:21 +0000

    Add extensions

--- src/app.js
+++ src/app.js
@@ -2,22 +2,16 @@ import "bootstrap";
 import "./style.css";
 
 window.onload = function() {
-  //Set up arrays
   let pro = ["the", "our", "my", ""];
   let adj = ["great", "big", "cool", "cheesy", "funny", "spectacular"];
   let noun = ["jogger", "racoon", "giraffe", "witch", "tree", "cheese"];
+  let ext = [".com", ".net", ".io", ".es", ".tv", ".de"];
 
-  //create function to map and return a domain name
   let getDomName = arr => {
     let a = Math.floor(Math.random() * arr.length);
     return arr[a];
   };
 
   document.getElementById("theId").innerHTML =
-    getDomName(pro) + getDomName(adj) + getDomName(noun) + ".com";
-
-  //console.log(domGenFunction(pro, adj, noun).join("\n"));
-  //domGen.innerHTML = domGenFunction(pro, adj, noun).join("<br/>");
-
-  //document.getElementById("theId").innerHTML = dom;
+    getDomName(pro) + getDomName(adj) + getDomName(noun) + getDomName(ext);
 };
