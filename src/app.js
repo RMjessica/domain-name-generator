e0a089c169671c99075b5d994cdb2f1e3d62271b
RMjessica <jeess.rm1@gmail.com>
2022-06-27 13:51:09 +0000

    Fix to generate all possible combos

--- src/app.js
+++ src/app.js
@@ -3,15 +3,29 @@ import "./style.css";
 
 window.onload = function() {
   let pro = ["the", "our", "my", ""];
-  let adj = ["great", "big", "cool", "cheesy", "funny", "spectacular"];
+  let adj = ["great", "big", "cool", "cheesy", "funny", "rad"];
   let noun = ["jogger", "racoon", "giraffe", "witch", "tree", "cheese"];
   let ext = [".com", ".net", ".io", ".es", ".tv", ".de"];
 
-  let getDomName = arr => {
-    let a = Math.floor(Math.random() * arr.length);
-    return arr[a];
+  let getDomName = (...args) => {
+    let domList = "";
+    for (let i = 0; i < pro.length; i++) {
+      for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < noun.length; k++) {
+          for (let l = 0; l < ext.length; l++) {
+            domList +=
+              "<div class = 'alert alert-dark col mx-2 align-items-center'>" +
+              pro[i] +
+              adj[j] +
+              noun[k] +
+              ext[l] +
+              "</div>";
+          }
+        }
+      }
+    }
+    return domList;
   };
 
-  document.getElementById("theId").innerHTML =
-    getDomName(pro) + getDomName(adj) + getDomName(noun) + getDomName(ext);
+  document.getElementById("theId").innerHTML = getDomName(pro, adj, noun, ext);
 };
